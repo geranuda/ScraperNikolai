a5195aac8bb6660e4b85870b7253088e120c7136
geranuda <gernundav@gmail.com>
2022-01-10 12:51:58 -0600

    Before setting database temp and attemp to set podios APIS

---
+++ apiPodio.js
@@ -0,0 +1,18 @@
+//client id : stallion-engine-v8-automatization
+//client secret: uI2YKmUwKVW7gwslrjiOuiBRAhqTjwOrUM8Cy7EBRscZIrm03vywG3h6pMLa7sMa
+// Example config file where you might store your credentials
+var Podio = require('podio-js');
+const config = {
+    clientId: "stallion-engine-v8-automatization", 
+    clientSecret: "uI2YKmUwKVW7gwslrjiOuiBRAhqTjwOrUM8Cy7EBRscZIrm03vywG3h6pMLa7sMa",
+    appId: 25804586,
+    appToken: "a9c9aca5470b84d943f7a9b2f1bafd26"
+}
+
+// 👈 Authenticate with podio api to get access_token
+// 👈 Use axios to make api request to get items data
+// 👈 return the data for processing
+
+
+// 👈 Setup a local database and save the data from index.js 
+// 👈 
